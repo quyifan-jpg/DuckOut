b5e747aad28d9499ad3fcc5466421dd48f8f71f7
Yifan32 <Yifan32@node1.expduckdb.fardatalab-pg0.apt.emulab.net>
2025-03-04 12:13:01 -0700

    temp 1

--- rdma_client.c
+++ rdma_client.c
@@ -13,8 +13,10 @@ static int connect_to_server(const char *server_ip) {
     if (inet_pton(AF_INET, server_ip, &addr.sin_addr) <= 0)
         die("inet_pton");
 
-    if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0)
-        die("connect");
+    if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        fprintf(stderr, "Connect failed: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
 
     return sock;
 }
@@ -26,6 +28,12 @@ int main(int argc, char *argv[]) {
     }
 
     const char *server_ip = argv[1];
+    ucp_context_h context = NULL;
+    ucp_worker_h worker = NULL;
+    ucp_ep_h ep = NULL;
+    void *local_addr = NULL;
+    void *remote_addr = NULL;
+    int sock = -1;
 
     /* ----------------- UCX Initialization ----------------- */
     ucp_params_t ucp_params = {0};
@@ -35,72 +43,71 @@ int main(int argc, char *argv[]) {
     status = ucp_config_read(NULL, NULL, &config);
     if (status != UCS_OK) {
         fprintf(stderr, "Failed to read UCX config\n");
-        exit(EXIT_FAILURE);
+        goto cleanup;
     }
 
     ucp_params.field_mask = UCP_PARAM_FIELD_FEATURES;
     ucp_params.features   = UCP_FEATURE_TAG;
 
-    ucp_context_h context;
     status = ucp_init(&ucp_params, config, &context);
-    if (status != UCS_OK) {
-        fprintf(stderr, "Failed to initialize UCX\n");
-        exit(EXIT_FAILURE);
-    }
     ucp_config_release(config);
+    if (status != UCS_OK) {
+        fprintf(stderr, "Failed to init UCX context\n");
+        goto cleanup;
+    }
 
     ucp_worker_params_t worker_params = {0};
     worker_params.field_mask = UCP_WORKER_PARAM_FIELD_THREAD_MODE;
     worker_params.thread_mode = UCS_THREAD_MODE_SINGLE;
 
-    ucp_worker_h worker;
     status = ucp_worker_create(context, &worker_params, &worker);
     if (status != UCS_OK) {
-        fprintf(stderr, "Failed to create UCX worker\n");
-        exit(EXIT_FAILURE);
+        fprintf(stderr, "Failed to create worker\n");
+        goto cleanup;
     }
 
-    /* Get local worker address */
-    ucp_address_t *local_addr;
+    /* Get worker address */
     size_t local_addr_len;
-    status = ucp_worker_get_address(worker, &local_addr, &local_addr_len);
+    status = ucp_worker_get_address(worker, (ucp_address_t **)&local_addr, &local_addr_len);
     if (status != UCS_OK) {
         fprintf(stderr, "Failed to get worker address\n");
-        exit(EXIT_FAILURE);
+        goto cleanup;
     }
 
-    /* ----------------- Address Exchange ----------------- */
-    int sock = connect_to_server(server_ip);
-    printf("Connected to server %s:%d\n", server_ip, SERVER_PORT);
-
-    // Receive server's worker address
-    uint32_t server_addr_len;
-    recv_all(sock, &server_addr_len, sizeof(server_addr_len));
-    server_addr_len = ntohl(server_addr_len);
-    void *remote_addr = malloc(server_addr_len);
-    if (!remote_addr)
-        die("malloc");
-    recv_all(sock, remote_addr, server_addr_len);
-
-    // Send our worker address
+    /* Exchange addresses */
+    sock = connect_to_server(server_ip);
+    
+    // Send our address length and address
     uint32_t net_len = htonl(local_addr_len);
     send_all(sock, &net_len, sizeof(net_len));
     send_all(sock, local_addr, local_addr_len);
+
+    // Receive server's address
+    uint32_t server_addr_len;
+    recv_all(sock, &server_addr_len, sizeof(server_addr_len));
+    server_addr_len = ntohl(server_addr_len);
+    
+    remote_addr = malloc(server_addr_len);
+    if (!remote_addr) {
+        fprintf(stderr, "Failed to allocate memory for remote address\n");
+        goto cleanup;
+    }
+    recv_all(sock, remote_addr, server_addr_len);
     close(sock);
+    sock = -1;
 
     /* Create endpoint */
     ucp_ep_params_t ep_params = {0};
     ep_params.field_mask = UCP_EP_PARAM_FIELD_REMOTE_ADDRESS;
     ep_params.address    = remote_addr;
-    ucp_ep_h ep;
+
     status = ucp_ep_create(worker, &ep_params, &ep);
     if (status != UCS_OK) {
         fprintf(stderr, "Failed to create endpoint\n");
-        exit(EXIT_FAILURE);
+        goto cleanup;
     }
-    free(remote_addr);
 
-    /* ----------------- Send Message ----------------- */
+    /* Send the message */
     char send_buffer[MAX_MESSAGE];
     memset(send_buffer, 'A', MAX_MESSAGE - 1);
     send_buffer[MAX_MESSAGE - 1] = '\0';
@@ -113,23 +120,40 @@ int main(int argc, char *argv[]) {
                                                 TAG, &param);
     if (UCS_PTR_IS_ERR(send_req)) {
         fprintf(stderr, "Failed to send message\n");
-        exit(EXIT_FAILURE);
+        goto cleanup;
     }
 
     if (send_req != NULL) {
         while (ucp_request_check_status(send_req) == UCS_INPROGRESS) {
-            progress_worker(worker);
+            ucp_worker_progress(worker);
         }
         ucp_request_free(send_req);
     }
 
-    printf("Sent RDMA message of size %d bytes\n", MAX_MESSAGE);
+    printf("Message sent successfully\n");
 
-    /* Clean up */
-    ucp_ep_destroy(ep);
-    ucp_worker_release_address(worker, local_addr);
-    ucp_worker_destroy(worker);
-    ucp_cleanup(context);
+    /* Ensure the message is delivered before cleanup */
+    ucp_worker_flush(worker);
+
+cleanup:
+    if (ep != NULL) {
+        ucp_ep_destroy(ep);
+    }
+    if (local_addr != NULL) {
+        ucp_worker_release_address(worker, local_addr);
+    }
+    if (remote_addr != NULL) {
+        free(remote_addr);
+    }
+    if (worker != NULL) {
+        ucp_worker_destroy(worker);
+    }
+    if (context != NULL) {
+        ucp_cleanup(context);
+    }
+    if (sock >= 0) {
+        close(sock);
+    }
 
     return 0;
 }
